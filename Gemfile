bd7f817ea35a8873e9bae1d9537ac76d3c60d3eb
Ines Louro <inespazlouro@gmail.com>
2021-09-01 14:42:49 +0100

    [add] user can only edit and destroy places we created

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'bootsnap', '>= 1.4.4', require: false
 gem 'autoprefixer-rails', '10.2.5'
 gem 'font-awesome-sass'
 gem 'simple_form'
+gem 'pundit'
 
 group :development, :test do
   gem 'pry-byebug'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,18 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  include Pundit
+
+  after_action :verify_authorized, except: :index, unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
   skip_before_action :authenticate_user!, only: :home
 
   def home
+  end
+
+  private
+
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
 end
