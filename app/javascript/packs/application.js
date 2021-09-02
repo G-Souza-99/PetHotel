5c3437d999f011837ace2aad562bdaca3cc93573
Gabriel Souza <gsouzaa4@gmail.com>
2021-09-02 12:45:31 +0100

    [add] full responsive mobile booking page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,8 +24,8 @@ import "bootstrap";
 // import { initSelect2 } from '../components/init_select2';
 import { initTyped } from "./plugins/init_typedjs";
 
+
 document.addEventListener("turbolinks:load", () => {
   // Call your functions here, e.g:
-  // initSelect2();
   initTyped();
 });
