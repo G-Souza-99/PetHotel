579ffb81b370e824f396067e258a8a3034fbfc68
Gabriel Souza <gsouzaa4@gmail.com>
2021-09-03 16:55:46 +0100

    small fix

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -12,7 +12,7 @@ class PlacesController < ApplicationController
       lat: @place.latitude,
       lng: @place.longitude,
       info_window: render_to_string(partial: "info_window", locals: { place: @place }),
-      image_url: helpers.asset_url('heart')
+      image_url: helpers.asset_url('heart.png')
     }]
   end
 
