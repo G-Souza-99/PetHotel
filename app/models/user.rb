06a8f1fea01adeb6a52dbc70b87c07833dd52f9e
Gabriel Souza <gsouzaa4@gmail.com>
2021-09-05 23:07:20 +0100

    quick fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :places, dependent: :destroy
-  has_many :orders
+  has_many :orders, dependent: :destroy
 
   validates :email, presence: true
 end
